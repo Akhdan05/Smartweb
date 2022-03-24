4cff4e99323f716e937884e4720b90bb1ffdb874
fridawardani <fridaekawardanii@gmail.com>
2022-03-24 17:56:34 +0700

    pembenaran css

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -80,15 +80,15 @@
 </footer>
 </div>
 
-<script src="<?= base_url('assets/'); ?>js/jquery-3.3.1.min.js"></script>
-<script src="<?= base_url('assets/'); ?>js/jquery-ui.js"></script>
-<script src="<?= base_url('assets/'); ?>js/popper.min.js"></script>
-<script src="<?= base_url('assets/'); ?>js/bootstrap.min.js"></script>
-<script src="<?= base_url('assets/'); ?>js/owl.carousel.min.js"></script>
-<script src="<?= base_url('assets/'); ?>js/jquery.magnific-popup.min.js"></script>
-<script src="<?= base_url('assets/'); ?>js/aos.js"></script>
+<script src="<?php echo base_url() ?>assets/js/jquery-3.3.1.min.js"></script>
+<script src="<?php echo base_url() ?>assets/js/jquery-ui.js"></script>
+<script src="<?php echo base_url() ?>assets/js/popper.min.js"></script>
+<script src="<?php echo base_url() ?>assets/js/bootstrap.min.js"></script>
+<script src="<?php echo base_url() ?>assets/js/owl.carousel.min.js"></script>
+<script src="<?php echo base_url() ?>assets/js/jquery.magnific-popup.min.js"></script>
+<script src="<?php echo base_url() ?>assets/js/aos.js"></script>
 
-<script src="<?= base_url('assets/'); ?>js/main.js"></script>
+<script src="<?php echo base_url() ?>assets/js/main.js"></script>
 
 </body>
 
