fb03380c9ffde13afd4c2b2268b14b6563c23a75
Raychal <abelrawz@gmail.com>
2022-04-12 20:29:15 +0700

    fix date in notification

--- application/views/Shop/detail.php
+++ application/views/Shop/detail.php
@@ -1,7 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <title>Shoppers &mdash; Colorlib e-Commerce Template</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
@@ -33,20 +32,7 @@
             <h2 class="text-black"><?= $data['nama']; ?></h2>
             <p class="mb-4"><?= $data['deskripsi']; ?></p>
             <p><strong class="text-primary h4"><?= $data['harga']; ?></strong></p>
-            <div class="mb-5">
-              <div class="input-group mb-3" style="max-width: 120px;">
-              <div class="input-group-prepend">
-                <button class="btn btn-outline-primary js-btn-minus" type="button">&minus;</button>
-              </div>
-              <input type="text" class="form-control text-center" value="1" placeholder="" aria-label="Example text with button addon" aria-describedby="button-addon1">
-              <div class="input-group-append">
-                <button class="btn btn-outline-primary js-btn-plus" type="button">&plus;</button>
-              </div>
-            </div>
-
-            </div>
-            <p><a href="cart.html" class="buy-now btn btn-sm btn-primary">Add To Cart</a></p>
-
+            <p><a href="<?= $data['produk-href']; ?>" class="buy-now btn btn-sm btn-primary">Order</a></p>
           </div>
         </div>
       </div>
