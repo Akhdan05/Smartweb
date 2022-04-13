528a22f5f481bd841dde4d6a56e432d156d641e5
fridawardani <fridaekawardanii@gmail.com>
2022-04-13 16:47:49 +0700

    fixing

--- application/views/Shop/index.php
+++ application/views/Shop/index.php
@@ -31,15 +31,17 @@
                     </div>
                 </div>
                 <div class="row mb-5">
-                <?php foreach($data_shop as $data): ?>
+                    <?php foreach ($data_shop as $data) : ?>
                     <div class="col-sm-6 col-lg-4 mb-4" data-aos="fade-up">
                         <div class="block-4 text-center border">
                             <figure class="block-4-image">
-                                <a href="<?php echo site_url('shop/detail/'.$data['id']);?>"><img src="<?= $data['img-src']; ?>" alt="Image placeholder"
-                                        class="img-fluid"></a>
+                                <a href="<?php echo site_url('shop/detail/' . $data['id']); ?>"><img
+                                        src="<?= $data['img-src']; ?>" alt="Image placeholder" class="img-fluid"></a>
                             </figure>
                             <div class="block-4-text p-4">
-                                <h3><a href="<?php echo site_url('shop/detail/'.$data['id']);?>"><?= $data['nama']; ?></a></h3>
+                                <h3><a
+                                        href="<?php echo site_url('shop/detail/' . $data['id']); ?>"><?= $data['nama']; ?></a>
+                                </h3>
                                 <p class="mb-0"><?= $data['ulasan']; ?></p>
                                 <p class="text-primary font-weight-bold"><?= $data['harga']; ?></p>
                             </div>
@@ -53,9 +55,11 @@
                 <div class="border p-4 rounded mb-4">
                     <h3 class="mb-3 h6 text-uppercase text-black d-block">Categories</h3>
                     <ul class="list-unstyled mb-0">
-                        <li class="mb-1"><a href="<?php echo site_url('Shop/read_iphone');?>" class="d-flex"><span>Iphone</span> <span
+                        <li class="mb-1"><a href="<?php echo site_url('Shop/read_iphone'); ?>"
+                                class="d-flex"><span>Iphone</span> <span
                                     class="text-black ml-auto">(<?= $data_iphone ?>)</span></a></li>
-                        <li class="mb-1"><a href="<?php echo site_url('Shop/read_macbook'); ?>" class="d-flex"><span>Mac</span> <span
+                        <li class="mb-1"><a href="<?php echo site_url('Shop/read_macbook'); ?>"
+                                class="d-flex"><span>MacBook</span> <span
                                     class="text-black ml-auto">(<?= $data_macbook ?>)</span></a></li>
                     </ul>
                 </div>
